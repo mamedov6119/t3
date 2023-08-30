51bb3b17e77b5207466a5a34bfd3f9705f59ee63
Murad Mammadov <ylin2v@inf.elte.hu>
2023-08-30 17:20:58 +0200

    Loading spinner component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/index.tsx'],
+  content: ['./src/pages/index.tsx', './src/components/**/*.tsx'],
   theme: {
     extend: {},
   },
