c4d79532915068121a0a45226371493c0eb2403c
Murad Mammadov <ylin2v@inf.elte.hu>
2023-08-30 18:57:44 +0200

    New files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import {
   SignInButton,
   SignOutButton,
   SignedOut,
+  auth,
   useUser,
 } from "@clerk/nextjs";
 import Head from "next/head";
@@ -15,6 +16,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -96,8 +98,12 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
-          <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{`· ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content} </span>
       </div>
@@ -120,7 +126,7 @@ const Feed = () => {
   );
 };
 
-export default function Home() {
+export default function SinglePostPage() {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
   // start fetching asap
   api.posts.getAll.useQuery();
