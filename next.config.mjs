e6c5d84d9d2fd6713506d42868106471e6337eac
Murad Mammadov <ylin2v@inf.elte.hu>
2023-08-30 16:38:34 +0200

    Next image tag and style up

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  images: {
+    domains: ["img.clerk.com"],
+  },
+  
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
